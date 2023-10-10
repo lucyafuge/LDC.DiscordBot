292d288994cc0487d48f3fa3f35b9167fc09bf5c
Чешихин Артем <a.cheshihin@websoft.ru>
2023-10-11 01:33:11 +0300

    pre alpha: 1.0.0

--- LDC.DiscordBot.py
+++ LDC.DiscordBot.py
@@ -1,22 +1,30 @@
 ﻿import discord
 import clr
 import sys
+import os
 from discord.colour import Color
 from discord.embeds import Embed
 from discord.ext import commands
 from decouple import config
 from discord import app_commands
 
+
+curDir = os.path.dirname(os.path.abspath(__file__))
+curDirs = os.path.split(curDir)
+ldcDir = curDirs[len(curDirs) - 2]
+
 clr.AddReference(r"System")
-clr.AddReference(r"D:\Programs\LDC\LDC.ClassesLibrary\obj\Debug\LDC.ClassesLibrary.dll")
-clr.AddReference(r"D:\Programs\LDC\LDC.ClassesLibrary\packages\EntityFramework.6.4.4\lib\net45\EntityFramework.dll")
-clr.AddReference(r"D:\Programs\LDC\LDC.ClassesLibrary\packages\EntityFramework.6.4.4\lib\net45\EntityFramework.SqlServer.dll")
+clr.AddReference(os.path.join(ldcDir, r"LDC.ClassesLibrary\obj\Debug\LDC.ClassesLibrary.dll"))
+clr.AddReference(os.path.join(ldcDir, r"LDC.ClassesLibrary\packages\EntityFramework.6.4.4\lib\net45\EntityFramework.dll"))
+clr.AddReference(os.path.join(ldcDir, r"LDC.ClassesLibrary\packages\EntityFramework.6.4.4\lib\net45\EntityFramework.SqlServer.dll"))
 
 from LDC.ClassesLibrary import *
 from System.Collections.Generic import *
 
 
 token = config('token',default='')
+
+
 class Bot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
@@ -40,11 +48,12 @@ bot = Bot()
 
 @bot.hybrid_command(name = "sign", with_app_command = True, description = "Sign")
 async def test(ctx: commands.Context, flags: SignFlags):
-    lib = EnoaLibrary()
+    #Второй параметр поставить в True, если первый запуск
+    lib = EnoaLibrary(None, False)
+    color = discord.Color
     signsResponce = lib.GetSign(flags.bunti_1, flags.ayur_2, flags.dodor_3, flags.takhar_4)
     sign = signsResponce.Result
     if(sign != None):
-        color = discord.Color
         desc = f"\
         Кости: ({sign.Bunti}, {sign.Ayur}, {sign.Dodor}, {sign.Takhar}) \n\n \
         Сложность: {sign.Difficult} \n\n \
@@ -55,7 +64,6 @@ async def test(ctx: commands.Context, flags: SignFlags):
         "
         embed = Embed(title=sign.Name, color=color.blue(), description=desc)
     else:
-        color = discord.Color
         embed = Embed(title="Ошибка", color=color.red(), description="Не было обнаружено знамение по переданным данным")
 
     await ctx.defer(ephemeral = False)
