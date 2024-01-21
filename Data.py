2a456aba61391ff3f34352e0638fc1f27757a094
Чешихин Артем <a.cheshihin@websoft.ru>
2024-01-22 01:38:02 +0300

    Twitch Update

---
+++ Data.py
@@ -0,0 +1,7 @@
+from decouple import config
+
+TokenDiscord = config('TokenDiscord',default='')
+TwitchAccessToken = config('TwitchAccessToken',default='')
+TwitchClientID = config('TwitchClientID',default='')
+TwichSecretKey = config('TwichSecretKey',default='')
+TwichUsersLoggins = config('TwichUsersLoggins',default='')
